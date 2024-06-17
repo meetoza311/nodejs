36224d2d6030c2bc14a6ce7b164c14fddafc7486
Meet Oza <52732061+meetoza311@users.noreply.github.com>
2024-06-17 11:25:41 +0530

    sd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes'); // Correct path
-const sequelize = require('./config/database');
+// const sequelize = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 const builderRoutes = require('./builderRoutes');
 
@@ -24,33 +24,33 @@ app.get('/api/health', (req, res) => {
 // });
 
 // Routes
-app.use('/api/users', userRoutes); // Correct path for users
-app.use('/api/auth', authRoutes);
-app.use('/api/builders', builderRoutes);
+// app.use('/api/users', userRoutes); // Correct path for users
+// app.use('/api/auth', authRoutes);
+// app.use('/api/builders', builderRoutes);
 
 // Error handling for invalid JSON payloads
-app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-    console.error('Invalid JSON payload:', err);
-    return res.status(400).json({ message: 'Invalid JSON payload' });
-  }
-  next(err);
-});
+// app.use((err, req, res, next) => {
+//   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+//     console.error('Invalid JSON payload:', err);
+//     return res.status(400).json({ message: 'Invalid JSON payload' });
+//   }
+//   next(err);
+// });
 
 // Global error handler
-app.use((err, req, res, next) => {
-  console.error('Stack trace:', err.stack);
-  res.status(500).json({ message: 'Internal Server Error' });
-});
+// app.use((err, req, res, next) => {
+//   console.error('Stack trace:', err.stack);
+//   res.status(500).json({ message: 'Internal Server Error' });
+// });
 
 // Sync the models with the database
-sequelize.sync()
-  .then(() => {
-    console.log('Database & tables created!');
-  })
-  .catch((error) => {
-    console.error('Error creating database & tables:', error);
-  });
+// sequelize.sync()
+//   .then(() => {
+//     console.log('Database & tables created!');
+//   })
+//   .catch((error) => {
+//     console.error('Error creating database & tables:', error);
+//   });
 
 // Start the server
 const PORT = process.env.PORT || 3001;
